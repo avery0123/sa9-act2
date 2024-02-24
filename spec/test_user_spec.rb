f00cd8cde32ee9bb16eee7c9fab21eca8f30ed4d
Avery Oliver <averyaoliver03@gmail.com>
2024-02-24 00:35:57 -0600

    folders

---
+++ spec/test_user_spec.rb
@@ -0,0 +1,21 @@
+require 'test_user.rb'
+
+RSpec.describe User do
+  describe "#log_in" do
+    it "logs the user in" do
+      # Fill in the test
+    end
+  end
+
+  describe "#log_out" do
+    it "logs the user out" do
+      # Fill in the test
+    end
+  end
+
+  describe "#username" do
+    it "returns the correct username" do
+      # Fill in the test
+    end
+  end
+end
